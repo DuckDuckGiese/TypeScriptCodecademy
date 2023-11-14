2aeb524ce3b5240898f8a0ac926651c4869b347a
Richard Giese <richardgiese@Richards-MacBook-Pro.local>
2023-11-13 23:25:55 -0600

    TypeMart Complete

--- Typemart/index.js
+++ Typemart/index.js
@@ -1,6 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var products_1 = require("./products");
-var productName = 'beanie';
+var productName = 'fanny pack';
+var shippingAddress = '742 Evergreen Terrace, Springfield IL 62629';
+var shipping;
+var taxPercent;
+var taxTotal;
+var total;
 var product = products_1.default.filter(function (product) { return product.name === productName; })[0];
 console.log(product);
+if (product.preOrder === 'true') {
+    console.log('We will send you a message when your product is on its way.');
+}
+if (Number(product.price) > 25) {
+    shipping = 0;
+    console.log('free shipping provided for products over $25');
+}
+else {
+    shipping = 5;
+}
+if (shippingAddress.match('New York')) {
+    taxPercent = 0.1;
+}
+else {
+    taxPercent = 0.05;
+}
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+console.log("\nProduct:  ".concat(product.name, "\nAddress:  ").concat(shippingAddress, "\nPrice:    $").concat(product.price, "\nTax:      $").concat(taxTotal.toFixed(2), "\nShipping: $").concat(shipping.toFixed(2), "\nTotal:    $").concat(total.toFixed(2), "\n"));
--- Typemart/index.ts
+++ Typemart/index.ts
@@ -1,7 +1,41 @@
 import products from './products'
 
-const productName: string = 'beanie';
+const productName: string = 'fanny pack';
+const shippingAddress: string = '742 Evergreen Terrace, Springfield IL 62629';
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
 
 const product = products.filter(product => product.name === productName)[0];
 
-console.log(product);
+console.log(product)
+
+if (product.preOrder === 'true') {
+    console.log('We will send you a message when your product is on its way.');
+}
+
+if(Number(product.price) > 25) {
+    shipping = 0;
+    console.log('free shipping provided for products over $25');
+} else {
+    shipping = 5;
+}
+
+if(shippingAddress.match('New York')) {
+    taxPercent = 0.1;
+} else {
+    taxPercent = 0.05;
+}
+
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+
+console.log(`
+Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}
+`)
